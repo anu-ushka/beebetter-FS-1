d8a5e4792f27bb086c6e3a9ff7576fd1f9e87dee
Vanshika <vanshika.connects@gmail.com>
2025-11-12 17:13:22 +0530

    pushing

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ const handleSubmit = async (e) => {
   e.preventDefault();
 
   try {
-    const res = await fetch("https://solosphere-fs-ycns.vercel.app//api/auth/login", {
+    const res = await fetch("https://solosphere-fs-ycns.vercel.app/api/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
